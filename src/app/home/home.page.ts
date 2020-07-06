2398132b4832c7c8160598ad831ecebe00980072
Mahmood Abu Awwad <abuawwadmahmood@gmail.com>
2020-07-06 13:00:29 +0300

    LoginUi

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, ViewChild, ElementRef } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+ 
 
+  Login:boolean;
+  validForm:boolean;
   constructor() {}
+  ngOnInit(): void {
+    this.Login=true;
+    this.validForm=true;
+  }
+  
+  onSubmit(form:NgForm){
+  
+  }
 
 }

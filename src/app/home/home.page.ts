25282a95de3f50a2606f180dbe79d7aa72837b13
Mahmood Abu Awwad <abuawwadmahmood@gmail.com>
2020-07-07 11:35:38 +0300

    Task Done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,3 @@
-import { UsersService } from '../services/models/users.service';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { User } from '../services/models/User.model';
@@ -10,99 +9,92 @@ import { User } from '../services/models/User.model';
 })
 export class HomePage implements OnInit {
  
-  users:User[]=[];
-  Login:boolean;
+  users:User[]=[{FirstName:'adminFirsttName',LastName:'adminLastName',email:'admin@admin',password:"admin123"},
+                {FirstName:'Mahmood',LastName:'AbuAwwad',email:'abuawwadmahmood@gmail.com',password:"123123"}];
+  page:number;   //1 login  2 sign up     3 dashboard 
   validForm:boolean;
   passwordIncorrect:boolean;
   emailIncorrect:boolean;
   emailFound:boolean;
   wrongPasswords:string;
-
-   STORAGE={user:User};
-  constructor(private usersService:UsersService) {}
-
+  localStorageFirstName:string;
+  localStorageLastName:string;
+  constructor() {}
   ngOnInit(): void {
-    this.Login=true;
+    // must handle reloading while logged in
+    this.page=1;
     this.validForm=true;
-    this.users=this.usersService.getUsers();  //preferd to be subscibred than return a list :(
     this.passwordIncorrect=false;
     this.emailIncorrect=false;
     this.emailFound=false;
     this.wrongPasswords='hidden';
-
-
   }
-  
   onSubmitLogin(form:NgForm){ //handle the sign in operation --
-    this.users=this.usersService.getUsers();
     const email= form.form.value.email;
     const password= form.form.value.password;
-    // console.log(email +' '+ password)
-
-    for(let user of this.users){ //validate if user is registered inside my dummy ata
-      if(user.email===email){
-        this.emailIncorrect=false;
-        if(user.password===password){
-          this.passwordIncorrect=false;
-          
-            //save data to local storage
-        }
-        else{
-          this.passwordIncorrect=true;
-        }
+    let LoginUser:User;
+    this.emailIncorrect=false;
+    this.passwordIncorrect=false;
+    LoginUser=this.users.find (x=> x.email===email);
+    if(LoginUser===undefined){
+      this.emailIncorrect=true;
+    } else{
+      if(LoginUser.password!==password){
+        this.passwordIncorrect=true;
       }
-      else{
-        this.emailIncorrect=true;
+      else{ 
+        localStorage.setItem('firstName',LoginUser.FirstName);
+        localStorage.setItem('lastName',LoginUser.LastName);
+        localStorage.setItem('email',LoginUser.email);
+        this.page=3;
+        this.localStorageFirstName=localStorage.getItem('firstName');
+        this.localStorageLastName=localStorage.getItem('lastName');
       }
     }
-
   }
-
   navigateSignUp(){
     this.emailFound=false;
     this.wrongPasswords='hidden';
-    this.Login=false;
-    this.users=this.usersService.getUsers();
-
+    this.page=2;
   }
-
-
-
-
-
   onSubmitSignUp(form:NgForm){
-    this.users=this.usersService.getUsers();
     //search first for email
-    const firstName= form.form.value.fname;
-    const lastName= form.form.value.lname;
+    let flag=true;
+    const FName= form.form.value.fname;
+    const LName= form.form.value.lname;
     const email= form.form.value.email;
     const password= form.form.value.password;
     const RePassword= form.form.value.RePassword;
     if(password===RePassword){
       this.wrongPasswords='hidden';
-
+      flag=true;
       for(let user1 of this.users){
         if(email===user1.email){
           this.emailFound=true;
-        }
-        else{
-          this.emailFound=false;
-          //do the sign Up
-          console.log('fname'+firstName + '  lname'+lastName + '  em  '+email );
+          flag=false;
         }
       }
     }
     else{
       this.wrongPasswords='visible';
-
+      flag=false;
+    }
+    if(flag===true){
+      this.users.push({FirstName:FName,LastName:LName,email:email,password:password});
+      this.page = 1;
     }
   }
   navigateLogin(){
-    this.Login=true;
+    this.page=1;
     this.validForm=true;
-    this.users=this.usersService.getUsers();
     this.passwordIncorrect=false;
     this.emailIncorrect=false;
-
+  }
+  onClickLogOut(){
+    localStorage.removeItem('firstName');
+    localStorage.removeItem('lastName');
+    localStorage.removeItem('email');
+    this.page=1;
   }
 }
+
--- src/app/services/models/users.service.ts
+++ src/app/services/models/users.service.ts
@@ -6,19 +6,9 @@ import { User } from './User.model';
 })
 export class UsersService implements OnInit{
 
-  users:User[]=[{FirstName:'adminFirsttName',LastName:'adminLastName',email:'admin@admin',password:"admin123"}];
   constructor() { }
   ngOnInit(): void {
   }
 
-
-  addUSer(user:User){
-    this.users.push(user);
-  }
-  getUsers(){
-    return this.users;
-  }
-
-  
   
 }
